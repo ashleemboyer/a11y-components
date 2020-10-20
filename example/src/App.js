7e21b04327ab8aba6a55ad8a9be09425185e3ac3
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-10-20 16:45:56 -0400

    Showing Input in example

--- example/src/App.js
+++ example/src/App.js
@@ -1,25 +1,11 @@
 import React, { useState } from 'react';
-import { Listbox } from 'a11y-components';
+import { Input } from 'a11y-components';
 import 'a11y-components/dist/index.css';
 
 const App = () => {
   const [favoriteGoldenGirl, setFavoriteGoldenGirl] = useState('rose_nylund');
 
-  return (
-    <Listbox
-      label="Who's your favorite Golden Girl?"
-      onChange={(e) => {
-        setFavoriteGoldenGirl(e);
-      }}
-      options={[
-        { id: 'blanche_devereaux', label: 'Blanche Devereaux' },
-        { id: 'rose_nylund', label: 'Rose Nylund' },
-        { id: 'sophia_petrillo', label: 'Sophia Petrillo' },
-        { id: 'dorothy_zbornak', label: 'Dorothy Zbornak' }
-      ]}
-      value={favoriteGoldenGirl}
-    />
-  );
+  return <Input label="Who's your favorite Golden Girl?" />;
 };
 
 export default App;

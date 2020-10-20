0adc08ad2e6b49bcf0b4d37c4db6b58b66e2e75d
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-10-20 17:47:12 -0400

    Showing Listbox in example

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-import { Input } from 'a11y-components';
+import React from 'react';
+import { Listbox } from 'a11y-components';
 import 'a11y-components/dist/index.css';
 
 const App = () => {
-  const [favoriteGoldenGirl, setFavoriteGoldenGirl] = useState('rose_nylund');
-
-  return <Input label="Who's your favorite Golden Girl?" />;
+  return <Listbox label='Test' options={[]} />;
 };
 
 export default App;

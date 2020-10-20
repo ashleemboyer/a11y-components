3383d6be8980712d36a63aae49cf4b5bdf48ed06
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-10-19 21:52:21 -0400

    Showing Listbox in example app

--- example/src/App.js
+++ example/src/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
-
-import { Input } from 'a11y-components';
+import { Listbox } from 'a11y-components';
 import 'a11y-components/dist/index.css';
 
 const App = () => {
-  const [firstName, setFirstName] = useState('Ashlee');
+  const [favoriteGoldenGirl, setFavoriteGoldenGirl] = useState('rose_nylund');
 
   return (
-    <Input
-      label='First name'
-      value={firstName}
+    <Listbox
+      label="Who's your favorite Golden Girl?"
       onChange={(e) => {
-        setFirstName(e.target.value);
+        setFavoriteGoldenGirl(e);
       }}
+      options={[
+        { id: 'blanche_devereaux', label: 'Blanche Devereaux' },
+        { id: 'rose_nylund', label: 'Rose Nylund' },
+        { id: 'sophia_petrillo', label: 'Sophia Petrillo' },
+        { id: 'dorothy_zbornak', label: 'Dorothy Zbornak' }
+      ]}
+      value={favoriteGoldenGirl}
     />
   );
 };

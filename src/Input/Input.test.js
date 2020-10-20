da35445b81812c4682ec5bebdcbb64fd11d82590
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-10-19 21:52:10 -0400

    Using better matcher in input tests

--- src/Input/Input.test.js
+++ src/Input/Input.test.js
@@ -6,7 +6,7 @@ import { Input } from '.';
 describe('<Input />', () => {
   it('renders an <input> element successfully', () => {
     const wrapper = shallow(<Input />);
-    expect(wrapper.find('input').length).toBe(1);
+    expect(wrapper.find('input')).toHaveLength(1);
   });
 
   describe('with the `label` prop', () => {
@@ -14,7 +14,7 @@ describe('<Input />', () => {
       const testLabel = 'First name';
       const wrapper = shallow(<Input label={testLabel} />);
       const label = wrapper.find('label');
-      expect(label.length).toBe(1);
+      expect(label).toHaveLength(1);
       expect(label.text()).toBe(testLabel);
     });
   });

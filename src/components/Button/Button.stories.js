ffe41ad0aa28488c9fdfca4b8613163f2faa6b83
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-12-13 17:30:25 -0500

    Rearranging imports

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Button } from '.';
 import { withKnobs, text } from '@storybook/addon-knobs';
+import { Button } from '.';
 
 export default {
   title: 'Button',

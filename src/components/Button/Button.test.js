ff93bcf5f6b7f5330cd21c95e817b3871b3f2571
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-12-13 09:43:48 -0500

    Updating Button tests

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
+import { fireEvent, render } from '@testing-library/react';
 import Button from '.';
 
 describe('<Button />', () => {
-  it('it renders its children successfully', () => {
-    const wrapper = shallow(<Button>hello</Button>);
-    expect(wrapper.find('button').text()).toBe('hello');
+  it('renders a native button element with the correct text', () => {
+    const { getByRole } = render(<Button>test</Button>);
+    expect(getByRole('button')).toHaveTextContent('test');
+  });
+
+  it('invokes onClick when clicked', () => {
+    const onClick = jest.fn();
+    const { getByText } = render(<Button onClick={onClick}>test</Button>);
+    fireEvent.click(getByText('test'));
+    expect(onClick).toHaveBeenCalled();
   });
 });

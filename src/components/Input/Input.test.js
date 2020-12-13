27f9073d8435b7941088a4b842980f12a3e33444
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-12-13 11:38:59 -0500

    Making tests work with scss and fixing Input tests

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,29 +1,20 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import Input from '.';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Input } from '.';
 
 describe('<Input />', () => {
-  it('runs', () => {
-    expect(true).toBeTruthy();
+  it('renders an <input>', () => {
+    const wrapper = shallow(<Input />);
+    expect(wrapper.find('input')).toHaveLength(1);
   });
 
-  // it('renders an <input>', () => {
-  //   const { getByPlaceholderText } = render(<Input placeholder='test' />);
-  //   expect(getByPlaceholderText('test')).toBeInTheDocument();
-  // });
-
-  // it('renders a <label> with given `label` prop', () => {
-  //   const { getByLabelText } = render(<Input id='test-input' label='test' />);
-  //   expect(getByLabelText('test')).not.toBeNull();
-  // });
-
-  // describe('with the `label` prop', () => {
-  //   it('renders a <label> element successfully', () => {
-  //     const testLabel = 'First name';
-  //     const wrapper = shallow(<Input label={testLabel} />);
-  //     const label = wrapper.find('label');
-  //     expect(label).toHaveLength(1);
-  //     expect(label.text()).toBe(testLabel);
-  //   });
-  // });
+  describe('with the `label` prop', () => {
+    it('renders a <label> element successfully', () => {
+      const testLabel = 'First name';
+      const wrapper = shallow(<Input label={testLabel} />);
+      const label = wrapper.find('label');
+      expect(label).toHaveLength(1);
+      expect(label.text()).toBe(testLabel);
+    });
+  });
 });

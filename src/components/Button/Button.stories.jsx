38d8f507a1cac75daa3716ed0352e77681e80248
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-02-09 23:46:03 -0500

    Adding basic button story

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,38 +1,13 @@
 import React from 'react';
-import { Button } from './Button';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Button from './Button';
 
-export default {
-  title: 'Button',
-  component: Button,
-  argTypes: {
-    children: {
-      description: 'The children to render inside of the button.',
-    },
-    onClick: {
-      description: 'Passed as the `<button>` onClick.',
-    },
-    style: {
-      control: {
-        type: 'object',
-      },
-      description: 'Used for dynamically generated inline styles.',
-    },
-  },
+export const text = 'Hello, Button!';
+export const actions = {
+  onClick: action('click'),
 };
 
-const Template = (args) => <Button {...args} />;
-
-export const Basic = Template.bind({});
-Basic.args = {
-  children: 'Click me!',
-  onClick: () => {
-    alert('The button was clicked!');
-  },
-  style: {
-    fontSize: '1rem',
-    padding: 8,
-    border: '1px solid black',
-    borderRadius: 8,
-    backgroundColor: 'lightblue',
-  },
-};
+storiesOf('Button', module).add('default', () => (
+  <Button onClick={actions.onClick}>{text}</Button>
+));
